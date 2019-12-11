139145e37d58b790419dca7e89839c2d0737a44b
Karli Steeves <steeves.karli@gmail.com>
2019-12-11 12:19:42 -0600

    done two exercises

---
+++ src/Karli/karliExercises.java
@@ -0,0 +1,7 @@
+public class karliExercises {
+//Write a function that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
+
+
+}
+
+
